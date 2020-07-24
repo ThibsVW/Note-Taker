55339fad962c58d64dac45c252d43ac4f4b9643b
ThibsVW <thibault.vanweydeveld@hotmail.com>
2020-07-25 06:57:05 +0800

    app.js fixed

--- app.js
+++ app.js
@@ -1,20 +1,52 @@
 const express = require("express");
 const app = express();
-
+const path = require('path');
+const fs = require('fs');
+const noteData = require('./public/db/db.json');
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static("public"));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-const apiRoutes = require("./public/routes/api");
-const htmlRoutes = require("./public/routes/htmljs");
+// Create routes the the HTML Pages using GET requests
 
-app.use(apiRoutes);
-app.use(htmlRoutes);
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+})
 
-app.listen(PORT, function(){
-    console.log("App listening on PORT: " + PORT);
-});
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+})
+
+app.get('/api/notes', (req, res) => {
+    res.json(noteData);
+})
+
+app.post('/api/notes', (req, res) => {
+    noteData.push(req.body);
+    noteData.forEach((note, i) => {
+        note.id = i + 1;
+    })
+    let newNote = JSON.stringify(noteData);
+    fs.writeFileSync('./public/db/db.json', newNote);
+
+    res.json(noteData);
+})
+
+app.delete('/api/notes/:id', (req, res) => {
+
+    let filtered = noteData.filter(note => note.id !== parseInt(req.params.id));
+    fs.writeFileSync('./public/db/db.json', JSON.stringify(filtered));
+
+    //alter the note data with the filtered results so when it sends the response back it is immediately showned on the front end
+    noteData = filtered;
+
+    res.json(noteData);
+
+})
+
+app.listen(PORT, () => {
+    console.log(`Listening on port: ${PORT}`);
+})
